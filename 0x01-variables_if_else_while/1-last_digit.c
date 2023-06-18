18b94d4b7290275de613208084097039a1aac576
kellyinfortech <kellyinfortech.solution@gmail.com>
2023-06-18 11:30:51 -0700

    program that will assing a random number

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -10,23 +10,24 @@
 int main(void)
 {
 	int n;
-	int md;
 
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
-	md = n % 10;
 
-	if (md > 5)
+	if ((n % 10) > 5)
 	{
-		printf("Last digit of %d is %d and is greater than 5\n", n, md);
+		printf("Last digit of %d is %d and is greater than 5\n",
+			n, n % 10);
 	}
-	else if (md == 0)
+	else if ((n % 10) < 6 && (n % 10) != 0)
 	{
-		printf("Last digit of %d is %d and is  0\n", n, md);
+		printf("Last digit of %d is %d and is less than 6 and not 0\n",
+			n, n % 10);
 	}
 	else
 	{
-		printf("Last digit of %d is %d and is less than 6 and not 0\n", n, md);
+		printf("Last digit of %d is %d and is 0\n",
+			n, n % 10);
 	}
 
 	return (0);
